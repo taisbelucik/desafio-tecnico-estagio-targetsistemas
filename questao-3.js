cac8d503b9c11a1354da92335efbd1ec589b6242
taisbelucik <taisbelucik@hotmail.com>
2023-03-06 16:24:36 -0300

    primeiro commit

---
+++ questao-3.js
@@ -0,0 +1,16 @@
+// 3) Descubra a lógica e complete o próximo elemento:
+
+// a) 1, 3, 5, 7, _9__
+
+// b) 2, 4, 8, 16, 32, 64, __128__
+
+// c) 0, 1, 4, 9, 16, 25, 36, __49__
+
+// d) 4, 16, 36, 64, _100___
+
+// e) 1, 1, 2, 3, 5, 8, __13__
+
+// f) 2,10, 12, 16, 17, 18, 19, _200___
+
+// RESPOSTA
+// Os próximos elementos das sequências são a) 9, b) 128, c) 49, d) 100, e) 13, f) 200.
