ea3186a07a070e7650c18d80ea5b251610731691
taisbelucik <taisbelucik@hotmail.com>
2023-03-06 21:03:07 -0300

    respondendo questão 2

--- questao-2.js
+++ questao-2.js
@@ -4,4 +4,26 @@
 
 // Esse número pode ser informado através de qualquer entrada de sua preferência ou pode ser previamente definido no código; -->
 
-
+//RESPOSTA
+const num = 2584;
+const isFibonacci = (num) => {
+  if (num === 0 || num === 1) {
+    return true;
+  }
+  let prev = 1;
+  let count = 2;
+  let temp = 0;
+  while (count <= num) {
+    if (prev + count === num) {
+      return true;
+    }
+    temp = prev;
+    prev = count;
+    count += temp;
+  }
+  return false;
+};
+console.log(isFibonacci(num));
+console.log(isFibonacci(6765));
+console.log(isFibonacci(45));
+console.log(isFibonacci(8767));
